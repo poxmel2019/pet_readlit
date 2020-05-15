d2919629a205c5e4fa2930b1f38b4b6052d743be
Madiyar <nu_ofsn11@mail.ru>
2020-05-15 13:14:23 +0600

    Philosphy file and code reducing

--- pet_readlit/work/analyze_and_predict.py
+++ pet_readlit/work/analyze_and_predict.py
@@ -8,14 +8,12 @@ def p():
 
     questions = {'1 + 1':2,'1 - 1':0,'1 * 1':1,'1 / 1':1}
     user_dict = {}
+    count = 0
+    
     for x in questions:#.items():
         user_answer = input(x+' = \n')
         user_dict[x] = user_answer
         
-        
-    count = 0
-    for x,y in user_dict.items():
-        print(x,y)
 
     print(count)
     
--- pet_readlit/work/collect_and_handle_data.py
+++ pet_readlit/work/collect_and_handle_data.py
@@ -7,29 +7,22 @@ path1 = 'D:\\pet_readlit\\pet_readlit\\work\\raw_lists\\'
 path2 = 'D:\\pet_readlit\\pet_readlit\\work\\'
 expa = '.txt'
 
-def best100books():
+def b100b():
+    '''b100b'''
+    # 1
 
+    file_list = retrieve_data_from_file(list_for_handling[0])
     
-    file = 'D:\\pet_readlit\\pet_readlit\\work\\raw_lists\\' + \
-    list_for_handling[0]+'.txt'
-    handling_file = open(file,encoding='utf-8')
-    handling_file_list = handling_file.readlines()
-    handling_file.close()
-
-   
     new_list = []
-    for x in handling_file_list:
+    for x in file_list:
         new_list.append(x[x.index(' - ')+3:].strip('\n'))
 
-    new_file = open('D:\\pet_readlit\\pet_readlit\\work\\best100books.txt','w')
-    fill_file(new_list,new_file)
-    new_file.close()
+    create_new_file(new_list,list_for_handling[0])
 
 def b100b2():
-
-    file = open(path1+list_for_handling[1]+expa,'r',encoding='utf-8')
-    file_list = file.readlines()
-    file.close()
+    '''b100b2'''
+    # 2
+    file_list = retrieve_data_from_file(list_for_handling[1])
 
     file_list.pop(len(file_list)-1)
 
@@ -41,36 +34,26 @@ def b100b2():
             file_list[i] = x[x.index('.')+2:x.rindex('.')]
         i += 1
             
-        
-
-    new_file = open(path2+'best100books2'+expa,'w',encoding='utf-8')
-    fill_file(file_list,new_file)
-    new_file.close()
-        
     show_list(file_list)
 
-def litres():
+    create_new_file(file_list,list_for_handling[1])
 
-    file = open(path1+list_for_handling[2]+expa,'r',encoding='utf-8')
-    file_list = file.readlines()
-    file.close()
+def litres():
+    '''litres'''
+    # 3
+    file_list = retrieve_data_from_file(list_for_handling[2])
 
     i = 0
     for x in file_list:
         file_list[i] = x[x.index('.')+2:x.index('(')]
         i += 1
 
-    new_file = open(path2+'litres'+expa,'w',encoding='utf-8')
-    fill_file(file_list,new_file)
-    new_file.close()
-        
     show_list(file_list)
-
+    
+    create_new_file(file_list,list_for_handling[2])
 def unfinished():
-
-    file = open(path1+list_for_handling[5]+expa,'r',encoding='utf-8')
-    file_list = file.readlines()
-    file.close()
+    # 4
+    file_list = retrieve_data_from_file(list_for_handling[5])
 
     file_list.pop(0)
     file_list.pop(0)
@@ -85,18 +68,15 @@ def unfinished():
             file_list[i] = x[x.index('.')+2:].strip('\n')
         i += 1
 
-    new_file = open(path2+'unfinished'+expa,'w',encoding='utf-8')
-    fill_file(file_list,new_file)
-    new_file.close()
-        
     show_list(file_list)
 
+    create_new_file(file_list,list_for_handling[5])
+    
+
 def philosophy():
-
-    file = open(path1+list_for_handling[4]+expa,'r',encoding='utf-8')
-    file_list = file.readlines()
-    file.close()
-
+    # 5
+   
+    file_list = retrieve_data_from_file(list_for_handling[4])
 
     file_list.pop(0)
     file_list.pop(len(file_list)-1)
@@ -105,18 +85,12 @@ def philosophy():
     for x in file_list:
         try:
             file_list[i] = x[x.index('«'):x.rindex('»')+1]
-            
         except ValueError:
             continue
         i += 1
-    #new_file = open(path2+'philosophy'+expa,'w',encoding='utf-8')
-    #fill_file(file_list,new_file)
-    #new_file.close()
 
     i = 0
     for x in file_list:
-        if i == 4:
-            continue
         file_list[i] = x.replace('«','"')
         i += 1
 
@@ -125,28 +99,38 @@ def philosophy():
         file_list[i] = x.replace('»','"')
         i += 1
 
+    file_list[4] = 'Боэций Аниций "Комментарий к «Категориям» Аристотеля"'
+  
     txt = []
     fili = []
     for x in file_list:
         txt = to_handle_string(x)
         for y in txt:
-            fili.append(y)
-    #show_list(file_list)
-    show_list(fili)
-    print(len(file_list))
-    print(len(fili))
+           fili.append(y)
+           
+    create_new_file(fili,list_for_handling[4])
+ 
+def retrieve_data_from_file(path_from_list):
     
+    file = open(path1+path_from_list+expa,'r',encoding='utf-8')
+    some_list = file.readlines()
+    file.close()
+    return some_list
 
+def create_new_file(some_list,name):
+    new_file = open(path2+name+expa,'w',encoding='utf-8')
+    fill_file(some_list,new_file)
+    new_file.close()
 
 def show_list(some_list):
     for x in some_list:
         print(x)
+        
 def fill_file(some_list, some_file):
     for x in some_list: some_file.write(x+'\n')
 
 def to_handle_string(string):
-
-   
+    
     quotes_pos = []
     txts = []
     k = 0
